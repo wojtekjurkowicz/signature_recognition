6521652828b73e68c90b743ca3babab341002ed8
wojtekjurkowicz <wojtek.jurkowicz@gmail.com>
2025-07-01 22:54:04 +0200

    fix

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,19 @@
 import numpy as np
+import os
 from scripts.models import train_and_evaluate
 
 
-def test_train_and_evaluate_runs():
+def test_train_and_evaluate_runs(tmp_path):
+    os.makedirs(tmp_path / "models", exist_ok=True)
+    os.makedirs(tmp_path / "outputs", exist_ok=True)
+
     X = np.random.rand(40, 25)
     y = np.array([0]*20 + [1]*20)
-    train_and_evaluate(X, y, model_name="test_run")
+
+    old_cwd = os.getcwd()
+    os.chdir(tmp_path)
+
+    try:
+        train_and_evaluate(X, y, model_name="test_run")
+    finally:
+        os.chdir(old_cwd)
